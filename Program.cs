46af25d50617cead6f69b2f5e0c58ec7f88f94d5
RISHABH <gguptarishabh82@gmail.com>
2022-03-21 13:36:24 +0530

    Calling in main

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList_Day14
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Linkedlist1 linkedlist = new Linkedlist1();
+
+            linkedlist.Add(56);
+            linkedlist.Add(30);
+            linkedlist.Add(70);
+            linkedlist.Display();
+            Console.ReadLine();
+        }
+    }
+}
